b6d88be626e4b73970f21eb7cd581d1436392b25
Ondrej Holesovsky <ondrejh@kth.se>
2015-10-21 16:24:41 +0200

    object_detector node finished and mostly tested. 6 images (320x240) per second. Input: topic of type sensor_msgs/Image. Output: topic of type detection/BoundingBoxArray.
    The classifier training shall be provided with more negative examples as there are many false positives (as expected for just 120 negative examples in the first training set).

--- ras_vision/detection/src/object_detector.cpp
+++ ras_vision/detection/src/object_detector.cpp
@@ -2,20 +2,36 @@
 #include "sensor_msgs/Image.h"
 #include "detection/LinearClassifier.h"
 #include "detection/MovingWindow.h"
+#include "detection/BoundingBox.h"
+#include "detection/BoundingBoxArray.h"
 
+int noThreads = 4;
 LinearClassifier *linClass;
 MovingWindow *movWin;
-CImg<my_float> *img;
+ros::Publisher *bb_pub;
 
-CImg<double> rosImg2CImg(sensor_msgs::Image rim)
+void rosImg2CImg(const sensor_msgs::Image::ConstPtr& rim, CImg<my_float> &_img)
 {
     //This is NOT tested! The data order (x,y,c) may be different.
-    unsigned char *_data = rim.data.data();
-    int _width = rim.width;
-    int _height = rim.height;
-    int _channels = rim.step/rim.width;
-    CImg<double> _img = CImg<unsigned char> (_data, _width, _height, 1, _channels);
-    return _img;
+    int _width = rim->width;
+    int _height = rim->height;
+    int _channels = rim->step/rim->width;
+
+    int size = _width*_height*_channels;
+    int s1 = _width*_height;
+    char * dataF2 = (char*) malloc(size);
+    int k = 0;
+    assert(_channels==3);
+
+    for(int i = 0; i<size; i+=_channels){
+        dataF2[k+0*s1] = rim->data[i+2];
+        dataF2[k+1*s1] = rim->data[i+1];
+        dataF2[k+2*s1] = rim->data[i+0];
+        k++;
+    }
+
+    _img = CImg<char> (dataF2, _width, _height, 1, _channels, false); //false for is_shared
+    free(dataF2);
 }
 
 void testLinearClassifier(void)
@@ -26,7 +42,7 @@ void testLinearClassifier(void)
         ROS_INFO("Linear classifier test FAILED.\n");
 }
 
-void runDetection(void)
+void runDetection(CImg<my_float> *img)
 {
     movWin->runWindow(*img, DATA_PATCHES);
     std::vector <int> pos = linClass->getPositive(movWin->getDataPatches(), 2);
@@ -35,6 +51,32 @@ void runDetection(void)
         bb = movWin->getBoundingBox(pos[i]);
         std::cout << "[" << bb[0] << " " << bb[1] << "; " << bb[2] << " " << bb[3] << "]" << std::endl;
     }
+}
+
+
+void processImage(const sensor_msgs::Image::ConstPtr& msg)
+{
+    CImg<my_float> img;
+    rosImg2CImg(msg, img);
+    img.resize(320, 240, 1, img.depth());
+
+    movWin->runWindow(img, DATA_PATCHES);
+    std::vector< std::vector<my_float> > dataPatches = movWin->getDataPatches();
+    std::vector <int> pos = linClass->getPositive(dataPatches, noThreads);
+    std::vector <int> bb;
+    detection::BoundingBox bb_msg;
+    detection::BoundingBoxArray bb_array_msg;
+    bb_array_msg.header = msg->header;
+    ROS_INFO("positives %lu, bb total count %lu\n", pos.size(), dataPatches.size());
+    for(int i = 0; i<pos.size(); i++){
+        bb = movWin->getBoundingBox(pos[i]);
+        bb_msg.x0 = bb[0];
+        bb_msg.y0 = bb[1];
+        bb_msg.x1 = bb[2];
+        bb_msg.y1 = bb[3];
+        bb_array_msg.bounding_boxes.push_back(bb_msg);
+    }
+    bb_pub->publish(bb_array_msg);
 }
 
 
@@ -53,18 +95,25 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "object_detector");
 	ros::NodeHandle n("/object_detector");
     
-    std::string modelFile, imageFile;
+    std::string modelFile, imageTestFile, imageTopic, bbTopic;
     n.param<std::string>("linear_classifier_model_file", modelFile, "");
-    n.param<std::string>("detector_test_image_file", imageFile, "");
-	
+    n.param<int>("linear_classifier_threads", noThreads, 1);
+    n.param<std::string>("detector_test_image_file", imageTestFile, "");
+    n.param<std::string>("detector_image_topic", imageTopic, "/camera/rgb/image_rect_color");
+    n.param<std::string>("detector_bounding_box_topic", bbTopic, "/object_detector/bounding_boxes");
+
     linClass = new LinearClassifier(modelFile.c_str());
-    
     int sc[] = {320, 240, 160, 120, 80, 60};
     std::vector<int> scales(sc, sc + sizeof(sc) / sizeof(int));
-    movWin = new MovingWindow(1, 3, 1, 5, scales);
-    img = new CImg<my_float> (imageFile.c_str());
-    //testLinearClassifier();
-    runDetection();
+    movWin = new MovingWindow(30, 30, 6, 6, scales);
+
+    ros::Publisher bb_pub_obj = n.advertise<detection::BoundingBoxArray>(bbTopic, 50);
+    bb_pub = &bb_pub_obj;
+    ros::Subscriber image_sub = n.subscribe<sensor_msgs::Image>(imageTopic, 2, processImage);
+	
+
+    //CImg<my_float> img = new CImg<my_float> (imageFile.c_str());
+    //testLinearClassifier()
     
     const int rate = 20;
     ros::Rate loop_rate(rate);
