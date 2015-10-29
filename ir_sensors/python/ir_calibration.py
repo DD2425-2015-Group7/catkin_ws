e6de410df3d60dd9d721a9a22782d3d8daf7ca28
Ondrej Holesovsky <ondrejh@kth.se>
2015-10-29 12:28:06 +0100

    ir_publish node for distance publishing in meters created. IR sensor calibration data format and process changed. Everything is essentially tested.

--- ir_sensors/python/ir_calibration.py
+++ ir_sensors/python/ir_calibration.py
@@ -3,29 +3,65 @@
 import random
 import csv
 import rospy
-import rosbag
 from ras_arduino_msgs.msg import ADConverter
 from math import *
 from pylab import *
 import threading
 
+class SensorDesc():
+    channel = ""
+    number = 0
+    t = 0
+    min_range = 0.0
+    max_range = 0.0
+    
+def loadSensorParams(sens):
+    polLen = rospy.get_param('~sensor_pol_coeffs', 0)
+    assert(polLen > 1)
+    assert(len(sens)>0)
+    coeffs = []
+    for i in range(0, len(sens)):
+        c = []
+        for j in range(0, polLen):
+            c += [rospy.get_param("~sensor{}_coeff{}".format(sens[i].number, j))]
+        coeffs += [c]
+    return coeffs
+    
+def loadSensorDesc():
+    count = rospy.get_param('~sensor_max_count', 0)
+    sens = []
+    for i in range(1, count+1):
+        t = rospy.get_param('~sensor{}_type'.format(i), 0)
+        if(t > 0):
+            assert(t == 1 or t == 2)
+            sensor = SensorDesc()
+            sensor.number = i
+            sensor.channel = "ch{}".format(i)
+            sensor.t = t
+            sensor.max_range = rospy.get_param('~type{}_range_max'.format(t), 0.0)
+            sensor.min_range = rospy.get_param('~type{}_range_min'.format(t), 0.0)
+            sens += [sensor]
+    return sens
+
 
 class SensorCalib():
-    yamlFile = ""
-    coeffs = []
-    sensorNumbers = []
-    sensorNames = []
-    lck = threading.Lock()
-
-    measureCount = 0
     noMes = 20
-    measuringEnabled = False
-    curMeasurement = []
-    curSenIdx = 0
     dstLong = [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8]
     dstShort = [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4]
     #dstLong = [0.2, 0.25, 0.3, 0.35]
     #dstShort = [0.1, 0.15, 0.2]
+    adcPoly = frange(75.0, 515.0, 1.0)
+    
+    coeffs = []
+    sensors = []
+    sensorNumbers = []
+    sensorNames = []
+    lck = threading.Lock()
+    measureCount = 0
+    measuringEnabled = False
+    curMeasurement = []
+    curSenIdx = 0
+    
         
     def readAdc(self, msg):
         self.lck.acquire()
@@ -37,7 +73,7 @@ class SensorCalib():
             self.lck.release()
             return
         self.measureCount += 1
-        self.curMeasurement += [getattr(msg, self.sensorNames[self.curSenIdx])]
+        self.curMeasurement += [getattr(msg, self.sensors[self.curSenIdx].channel)]
         self.lck.release()
         
     def wait4data(self):
@@ -51,13 +87,11 @@ class SensorCalib():
             self.lck.release()
             
     def showResult(self, pol, adc, dst):
-        adcPoly = frange(75.0, 515.0, 1.0)
-        dstPoly = polyval(pol, adcPoly)
-          
+        dstPoly = polyval(pol, self.adcPoly)
         #Plot everything.  
         pdatpen, = plot(adc, dst,'bx',
           label = "Measured".decode('utf-8'))
-        plot(adcPoly, dstPoly,
+        plot(self.adcPoly, dstPoly,
               'r-',label = "Regression".decode('utf-8'), linewidth = 3)
         ylabel('Distance [m]'.decode('utf-8'))
         xlabel('ADC value [-]')
@@ -67,23 +101,17 @@ class SensorCalib():
         show()
 
     def calibSensor(self, index):
-        n = -1
         self.curMeasurement = []
         dst = []
-        while n!=0 and n!=1:
-            print "Ready to calibrate IR sensor {}.\n".format(self.sensorNumbers[index])
-            print "Choose: 0 - short range, 1 - long range:"
-            try:
-                n = int(raw_input())
-            except ValueError:
-                print "Not a number, please try again.\n"
-                continue
-        if n==0:
+        print "Ready to calibrate IR sensor {}, type {}.\n".format(self.sensors[index].number, self.sensors[index].t)
+        print "Press Enter to proceed."
+        raw_input()
+        if self.sensors[index].t==1:
             vals = self.dstShort
         else:
             vals = self.dstLong
         for d in vals:
-            print "Place an object {} m from IR sensor {} and press Enter.\n".format(d, self.sensorNumbers[index])
+            print "Place an object {} m from IR sensor {} and press Enter.\n".format(d, self.sensors[index].number)
             raw_input()
             self.lck.acquire()
             self.measuringEnabled = True
@@ -102,25 +130,18 @@ class SensorCalib():
         with open(fileName, 'wb') as csvfile:
             awriter = csv.writer(csvfile, delimiter=' ')
             assert(len(self.coeffs)>=1)
-            awriter.writerow(["ir_sensor_pol_coeffs:", len(self.coeffs[0])])
-            for i in range(0, len(self.sensorNumbers)):
+            awriter.writerow(["sensor_pol_coeffs:", len(self.coeffs[0])])
+            for i in range(0, len(self.sensors)):
                 for j in range(0, len(self.coeffs[i])):
-                    awriter.writerow(["ir_sensor{}_coeff{}:".format(self.sensorNumbers[i], j), self.coeffs[i][j]])
-
-    def loadParams(self):
-        count = rospy.get_param('~ir_sensor_max_count', 0)
-        self.yamlFile = rospy.get_param('~ir_sensors_coeff_file', "")
-        for i in range(1, count+1):
-            if(rospy.get_param('~ir_sensor{}_enabled'.format(i), False) == True):
-                self.sensorNumbers += [i]
-                self.sensorNames += ["ch{}".format(i)]
+                    awriter.writerow(["sensor{}_coeff{}:".format(self.sensors[i].number, j), self.coeffs[i][j]])
                 
     def calibAll(self):
-        self.loadParams()
+        self.sensors = loadSensorDesc()
+        yamlFile = rospy.get_param('~sensors_coeff_file', "")
         rospy.Subscriber("/arduino/adc", ADConverter, self.readAdc)
-        for i in range(0, len(self.sensorNumbers)):
+        for i in range(0, len(self.sensors)):
             self.calibSensor(i)
-        self.saveYaml(self.yamlFile)
+        self.saveYaml(yamlFile)
 
 if __name__ == '__main__':
     rospy.init_node('ir_calibration')
---
+++ ir_sensors/python/ir_publish.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import random
+import csv
+import rospy
+from ras_arduino_msgs.msg import ADConverter
+from sensor_msgs.msg import Range
+from ir_sensors.msg import RangeArray
+from math import *
+from pylab import *
+from ir_calibration import SensorDesc, loadSensorDesc, loadSensorParams
+
+
+class SensorPublish():
+
+    coeffs = []
+    sensors = []
+    msg = RangeArray()
+    
+    def __init__(self):
+        self.irpub = rospy.Publisher("/ir_publish/sensors", RangeArray, queue_size = 5)
+        self.sensors = loadSensorDesc()
+        self.coeffs = loadSensorParams(self.sensors)
+        self.prepareArray()
+        rospy.Subscriber("/arduino/adc", ADConverter, self.readAdc)
+    
+    def prepareArray(self):
+        for i in range(0, len(self.sensors)):
+            r = Range()
+            r.min_range = self.sensors[i].min_range
+            r.max_range = self.sensors[i].max_range
+            r.radiation_type = r.INFRARED
+            self.msg.array += [r]
+        
+    def readAdc(self, msg):
+        for i in range(0, len(self.sensors)):
+            m = getattr(msg, self.sensors[i].channel)
+            self.msg.array[i].range = polyval(self.coeffs[i], m)
+        self.irpub.publish(self.msg)
+
+
+if __name__ == '__main__':
+    rospy.init_node('ir_publish')
+    
+    spub = SensorPublish()
+    rospy.spin()
+    
