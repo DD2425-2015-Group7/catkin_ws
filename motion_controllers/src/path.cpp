5b6be7d954f952288d0ebc79a487be79a8f88835
GsFevil <guisongfu@gmail.com>
2015-11-02 16:19:15 +0100

    test the Path following stuff

--- motion_controllers/src/path.cpp
+++ motion_controllers/src/path.cpp
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
   ros::Rate loop_rate(10);
   geometry_msgs::PoseStamped p1,p2;
   //geometry_msgs::Point po1;
-  p1.pose.position.x = 0.1;
+  p1.pose.position.x = 1.8;
   p1.pose.position.y = 0.2;
 
   p2.pose.position.x = 0.3;
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
   nav_msgs::Path path;
 
   path.poses.push_back(p1);
-  path.poses.push_back(p2);
+  //path.poses.push_back(p2);
 
   while (ros::ok())
   {
