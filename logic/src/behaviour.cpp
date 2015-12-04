6bfb992f82759e329ebac9d72961a1d9657da747
Ondrej Holesovsky <ondrejh@kth.se>
2015-12-04 19:18:36 +0100

    logic - shorter timeout for testing getting back.

--- logic/src/behaviour.cpp
+++ logic/src/behaviour.cpp
@@ -1,7 +1,7 @@
 #include "logic/FunctionBlocks.h"
 
 int safetyTime = 20; 
-const int explorationTimeout = 200/*300*/, fetchingTimeout = 180;
+const int explorationTimeout = 90/*300*/, fetchingTimeout = 180;
 geometry_msgs::Pose *startPose;
 FunctionBlocks *fb;
 const double radiusTolerance = 0.055, yawTolerance = 2*M_PI;
